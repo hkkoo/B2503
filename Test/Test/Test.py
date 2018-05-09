c1162f61ef933badd07f922ebd12a8c7ccc9ceaf
Hosung-Kim <tigerroaring@gmail.com>
2018-05-09 20:46:19 +0900

    New base

--- Test/Test/Test.py
+++ Test/Test/Test.py
@@ -1,25 +1,62 @@
-from pywinauto import application
-from pywinauto import timings
-import time
-import os
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import  *
+from PyQt5.QAxContainer import *
 
-app = application.Application()
-app.start("C:/KiwoomFlash2/bin/nkministarter.exe")
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-title = "번개 Login"
-dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
+        # Kiwoom Login
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom.dynamicCall("CommConnect()")
 
-pass_ctrl = dlg.Edit2
-pass_ctrl.SetFocus()
-pass_ctrl.TypeKeys('xxxx')
+        # OpenAPI+ Event
+        self.kiwoom.OnEventConnect.connect(self.event_connect)
+        self.kiwoom.OnReceiveTrData.connect(self.receive_trdata)
 
-cert_ctrl = dlg.Edit3
-cert_ctrl.SetFocus()
-cert_ctrl.TypeKeys('yyyy')
+        self.setWindowTitle("PyStock")
+        self.setGeometry(300, 300, 300, 150)
 
-btn_ctrl = dlg.Button0
-btn_ctrl.Click()
+        label = QLabel('종목코드: ', self)
+        label.move(20, 20)
 
-time.sleep(50)
-os.system("taskkill /im khmini.exe")
-time.sleep(50)
+        self.code_edit = QLineEdit(self)
+        self.code_edit.move(80, 20)
+        self.code_edit.setText("039490")
+
+        btn1 = QPushButton("조회", self)
+        btn1.move(190, 20)
+        btn1.clicked.connect(self.btn1_clicked)
+
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(10, 60, 280, 80)
+        self.text_edit.setEnabled(False)
+
+    def event_connect(self, err_code):
+        if err_code == 0:
+            self.text_edit.append("로그인 성공")
+
+    def btn1_clicked(self):
+        code = self.code_edit.text()
+        self.text_edit.append("종목코드: " + code)
+
+        # SetInputValue
+        self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+
+        # CommRqData
+        self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10001_req", "opt10001", 0, "0101")
+
+    def receive_trdata(self, screen_no, rqname, trcode, recordname, prev_next, data_len, err_code, msg1, msg2):
+        if rqname == "opt10001_req":
+            name = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, 0, "종목명")
+            volume = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, 0, "거래량")
+
+            self.text_edit.append("종목명: " + name.strip())
+            self.text_edit.append("거래량: " + volume.strip())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
