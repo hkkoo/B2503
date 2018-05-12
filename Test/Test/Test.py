da754ad689e83636abac2e9fde76a2eea0ac4f4d
Changki Kim <rlackdrl2000@naver.com>
2018-05-12 23:09:01 +0900

    add sample UI

--- Test/Test/Test.py
+++ Test/Test/Test.py
@@ -2,40 +2,27 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import  *
 from PyQt5.QAxContainer import *
+from PyQt5 import uic
 
-class MyWindow(QMainWindow):
+form_class = uic.loadUiType("ui/main_window.ui")[0]
+
+class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
-
+        
         # Kiwoom Login
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom.dynamicCall("CommConnect()")
 
         # OpenAPI+ Event
         self.kiwoom.OnEventConnect.connect(self.event_connect)
-        self.kiwoom.OnReceiveTrData.connect(self.receive_trdata)
+        #self.kiwoom.OnReceiveTrData.connect(self.receive_trdata)
 
-        self.setWindowTitle("PyStock")
-        self.setGeometry(300, 300, 300, 150)
-
-        label = QLabel('종목코드: ', self)
-        label.move(20, 20)
-
-        self.code_edit = QLineEdit(self)
-        self.code_edit.move(80, 20)
-        self.code_edit.setText("039490")
-
-        btn1 = QPushButton("조회", self)
-        btn1.move(190, 20)
-        btn1.clicked.connect(self.btn1_clicked)
-
-        self.text_edit = QTextEdit(self)
-        self.text_edit.setGeometry(10, 60, 280, 80)
-        self.text_edit.setEnabled(False)
+        self.setupUi(self)
 
     def event_connect(self, err_code):
         if err_code == 0:
-            self.text_edit.append("로그인 성공")
+            self.logText.append("로그인 성공")
 
     def btn1_clicked(self):
         code = self.code_edit.text()
